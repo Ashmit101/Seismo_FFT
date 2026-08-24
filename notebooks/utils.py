20a8d894cc011e2e4fb5d1efabb38f911c2dc81c
Ashmit Rajaure <ashmit.rajaure@gmail.com>
2026-08-24 14:24:37 +0545

    research: filtering the signal in notebook

---
+++ notebooks/utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fft import rfft, rfftfreq
+
+def plot_signal(time_data, motion_data, *, x_label = "Time", y_label):
+    fig, ax = plt.subplots()
+    ax.plot(time_data, motion_data, linewidth=1.0)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    plt.show()
+
+
+def plot_fft(motion_data, time_increment, *, y_label):
+    motion_spectrum = rfft(motion_data)
+    frequencies = rfftfreq(len(motion_data), d=time_increment)
+    amplitude = 2 / len(motion_data) * np.abs(motion_spectrum)
+    fig, ax = plt.subplots()
+    ax.plot(frequencies, amplitude, linewidth=1.0)
+
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel(y_label)
+    plt.show()
